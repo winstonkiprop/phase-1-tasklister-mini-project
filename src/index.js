41e58f9ba8b207065ae1784d450db58a64feeb84
winston kiprop <winstonekip50@gmail.com>
2022-06-14 21:52:02 +0300

    Done.

--- src/index.js
+++ src/index.js
@@ -1,3 +1,25 @@
 document.addEventListener("DOMContentLoaded", () => {
   // your code here
+  //get all DOM elements
+  const taskForm = document.getElementById('create-task-form')
+  const taskDescription = document.getElementById('new-task-description')
+
+  taskForm.addEventListener('submit', (e)=>{
+    e.preventDefault();
+    const newTask = document.createElement("li");
+    newTask.innerText = taskDescription.value;
+
+    appendNewTask(newTask);
+    removeElement();
+    e.target.reset()
+  })
 });
+
+const appendNewTask = task => {
+  document.getElementById("tasks").appendChild(task);
+}
+
+const removeElement = ()=> {
+  const liElement = document.querySelector("li").innerText = X;
+liElement.remove();
+}
